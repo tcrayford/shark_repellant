09bc12cbf15585567f9c9ece8dbc846ef857a273
Tom Crayford <tcrayford@googlemail.com>
2017-07-11 11:51:22 +0100

    fix all clippy feedback

--- src/main.rs
+++ src/main.rs
@@ -30,45 +30,44 @@ fn pwd() -> Result<String, Error> {
     match std::env::current_dir() {
         Ok(maybe_dir) => match maybe_dir.file_name() {
             Some(dir) => match dir.to_str() {
-                Some(dirname) => return Ok(format!("{}", dirname)),
-                None => return Err(git2::Error::from_str("")),
+                Some(dirname) => Ok(dirname.to_string()),
+                None => Err(git2::Error::from_str("")),
             },
-            None => return Err(git2::Error::from_str("")),
+            None => Err(git2::Error::from_str("")),
         },
-        Err(e) => return Err(git2::Error::from_str(&format!("{}", e)))
+        Err(e) => Err(git2::Error::from_str(&format!("{}", e)))
     }
 }
 
 fn git() -> Result<String, Error> {
     match Repository::discover(".") {
-        Ok(repo) => return git_info(repo),
-        Err(e) => return Err(e)
-    };
+        Ok(repo) => git_info(&repo),
+        Err(e) => Err(e)
+    }
 }
 
-fn git_info(repo: Repository) -> Result<String, Error> {
+fn git_info(repo: &Repository) -> Result<String, Error> {
     let state = repo.state();
     let statuses = format_statuses(repo.statuses(None));
     let head = format_head(repo.head());
     match state {
-        RepositoryState::Rebase => return format_rebase(rebase_info(repo), statuses, head, state),
-        RepositoryState::RebaseInteractive => return format_rebase(rebase_info(repo), statuses, head, state),
+        RepositoryState::Rebase | RepositoryState::RebaseInteractive => format_rebase(rebase_info(repo), &statuses[..], &head[..], state),
         _ => {
-            return Ok(format!("{}{}{}", head, statuses, format_state(state)))
+            Ok(format!("{}{}{}", head, statuses, format_state(state)))
         }
-    };
+    }
 }
 
-fn format_rebase(info: Result<RebaseInfo, Error>, statuses: String, head: String, state: RepositoryState) -> Result<String, Error> {
+fn format_rebase(info: Result<RebaseInfo, Error>, statuses: &str, head: &str, state: RepositoryState) -> Result<String, Error> {
     match info {
         Ok(r) => {
             match r.branch {
-                Some(branch) => return Ok(format!("{}{}|{} {}/{}", branch, statuses, r.rebase_type, r.step, r.total)),
-                None => return Ok(format!("{}|{} {}/{}", statuses, r.rebase_type, r.step, r.total)),
+                Some(branch) => Ok(format!("{}{}|{} {}/{}", branch, statuses, r.rebase_type, r.step, r.total)),
+                None => Ok(format!("{}|{} {}/{}", statuses, r.rebase_type, r.step, r.total)),
 
             }
         }
-        Err(e) => return Ok(format!("{}{}{}{}", e, head, statuses, format_state(state))),
+        Err(e) => Ok(format!("{}{}{}{}", e, head, statuses, format_state(state))),
     }
 }
 
@@ -76,22 +75,17 @@ fn format_state(state: RepositoryState) -> String {
     let state_string = match state {
         RepositoryState::Clean => "",
         RepositoryState::Merge => "MERGE",
-        RepositoryState::Revert => "REVERT",
-        RepositoryState::RevertSequence => "REVERT",
-        RepositoryState::CherryPick => "CHERRY-PICK",
-        RepositoryState::CherryPickSequence => "CHERRY-PICK",
+        RepositoryState::Revert | RepositoryState::RevertSequence => "REVERT",
+        RepositoryState::CherryPick | RepositoryState::CherryPickSequence => "CHERRY-PICK",
         RepositoryState::Bisect => "BISECT",
-        RepositoryState::Rebase => "REBASE",
-        RepositoryState::RebaseInteractive => "REBASE",
-        RepositoryState::RebaseMerge => "REBASE",
-        RepositoryState::ApplyMailbox => "MAILBOX",
-        RepositoryState::ApplyMailboxOrRebase => "MAILBOX",
+        RepositoryState::Rebase | RepositoryState::RebaseInteractive | RepositoryState::RebaseMerge => "REBASE",
+        RepositoryState::ApplyMailbox | RepositoryState::ApplyMailboxOrRebase => "MAILBOX",
     };
-    return String::from(state_string)
+    String::from(state_string)
 }
 
 fn format_head(head_result: Result<Reference, Error>) -> String {
-    return match head_result {
+    match head_result {
         Ok(head) => {
             if head.is_branch() {
                 let shorthand = head.shorthand();
@@ -100,7 +94,7 @@ fn format_head(head_result: Result<Reference, Error>) -> String {
                     None => String::from(""),
                 }
             } else {
-                match commit_shortid_from_reference(head) {
+                match commit_shortid_from_reference(&head) {
                     Ok(shortid) => shortid,
                     Err(_) => String::from("HEAD"),
                 }
@@ -110,22 +104,22 @@ fn format_head(head_result: Result<Reference, Error>) -> String {
     }
 }
 
-fn commit_shortid_from_reference(r: Reference) -> Result<String, Error> {
+fn commit_shortid_from_reference(r: &Reference) -> Result<String, Error> {
     let peeled = try!(r.peel(git2::ObjectType::Commit));
-    let _ = try!(peeled.as_commit().ok_or(git2::Error::from_str("HEAD")));
+    let _ = try!(peeled.as_commit().ok_or_else(|| git2::Error::from_str("HEAD")));
     let shortid = try!(peeled.short_id());
-    let shortid_str = try!(shortid.as_str().ok_or(git2::Error::from_str("HEAD")));
-    return Ok(format!("({}...)", shortid_str))
+    let shortid_str = try!(shortid.as_str().ok_or_else(|| git2::Error::from_str("HEAD")));
+    Ok(format!("({}...)", shortid_str))
 }
 
 fn format_statuses(statuses_result: Result<Statuses, Error>) -> String {
-    return match statuses_result {
-        Ok(statuses) => dirty_markers(statuses),
+    match statuses_result {
+        Ok(statuses) => dirty_markers(&statuses),
         Err(_) => String::from("")
     }
 }
 
-fn dirty_markers(statuses: Statuses) -> String {
+fn dirty_markers(statuses: &Statuses) -> String {
     let mut changes_in_index = false;
     let mut changes_in_workdir = false;
     let mut added_in_workdir = false;
@@ -174,7 +168,7 @@ fn dirty_markers(statuses: Statuses) -> String {
     if added_in_workdir {
         result = format!("{}{}", result, Red.paint("%").to_string())
     }
-    return result
+    result
 }
 
 #[derive(Copy,Clone)]
@@ -206,16 +200,16 @@ struct RebaseInfo {
     rebase_type: RebaseType,
 }
 
-fn rebase_info(repo: Repository) -> Result<RebaseInfo, Error> {
+fn rebase_info(repo: &Repository) -> Result<RebaseInfo, Error> {
     if repo.path().join("rebase-merge").exists() {
         let head_ref_name = try!(read_file_git_error(repo.path().join("rebase-merge").join("head-name").as_path()));
         let branch = format_head(repo.find_reference(&head_ref_name));
         let step = try!(read_file_git_error(repo.path().join("rebase-merge").join("msgnum").as_path()));
         let total = try!(read_file_git_error(repo.path().join("rebase-merge").join("end").as_path()));
         if repo.path().join("rebase-merge").join("interactive").exists() {
-            return Ok(RebaseInfo {branch: Some(branch), step: step, total: total, rebase_type: RebaseType::Interactive})
+            Ok(RebaseInfo {branch: Some(branch), step: step, total: total, rebase_type: RebaseType::Interactive})
         } else {
-            return Ok(RebaseInfo {branch: Some(branch), step: step, total: total, rebase_type: RebaseType::Merge})
+            Ok(RebaseInfo {branch: Some(branch), step: step, total: total, rebase_type: RebaseType::Merge})
         }
     } else if repo.path().join("rebase-apply").exists() {
         let head_ref_name = try!(read_file_git_error(repo.path().join("rebase-apply").join("head-name").as_path()));
@@ -223,21 +217,21 @@ fn rebase_info(repo: Repository) -> Result<RebaseInfo, Error> {
         let step = try!(read_file_git_error(repo.path().join("rebase-apply").join("next").as_path()));
         let total = try!(read_file_git_error(repo.path().join("rebase-apply").join("last").as_path()));
         if repo.path().join("rebase-apply").join("rebasing").exists() {
-            return Ok(RebaseInfo {branch: Some(branch), step: step, total: total, rebase_type: RebaseType::Plain})
+            Ok(RebaseInfo {branch: Some(branch), step: step, total: total, rebase_type: RebaseType::Plain})
         } else if repo.path().join("rebase-apply").join("applying").exists() {
-            return Ok(RebaseInfo {branch: Some(branch), step: step, total: total, rebase_type: RebaseType::ApplyMerge})
+            Ok(RebaseInfo {branch: Some(branch), step: step, total: total, rebase_type: RebaseType::ApplyMerge})
         } else {
-            return Ok(RebaseInfo {branch: Some(branch), step: step, total: total, rebase_type: RebaseType::ApplyMergeRebase})
+            Ok(RebaseInfo {branch: Some(branch), step: step, total: total, rebase_type: RebaseType::ApplyMergeRebase})
         }
     } else {
-        return Err(git2::Error::from_str("HEAD"))
+        Err(git2::Error::from_str("HEAD"))
     }
 }
 
 fn read_file_git_error(p: &Path) -> Result<String, git2::Error> {
     match read_file(p) {
         Ok(contents) => Ok(contents),
-        Err(e) => return Err(git2::Error::from_str(&format!("{}", e)))
+        Err(e) => Err(git2::Error::from_str(&format!("{}", e)))
     }
 }
 
@@ -245,5 +239,5 @@ fn read_file(p: &Path) -> Result<String, io::Error> {
     let mut file = try!(File::open(p));
     let mut contents = String::new();
     let _ = file.read_to_string(&mut contents);
-    return Ok(String::from(contents.trim()))
+    Ok(String::from(contents.trim()))
 }
